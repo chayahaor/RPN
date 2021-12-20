851a2bec97851056769bd750595c2d3f33a41f18
SaraYaffe <syaffe@student.touro.edu>
2021-12-19 20:33:59 -0500

    splash screen

--- app/src/main/java/com/example/rpn/SplashActivity.java
+++ app/src/main/java/com/example/rpn/SplashActivity.java
@@ -9,14 +9,17 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+//        ImageView image= (ImageView) findViewById (R.id.splash_screen);
+//        image.animate().rotation(2160f).setDuration(3000);
         AppCompatDelegate.setDefaultNightMode
                 (Build.VERSION.SDK_INT < 28 ? MODE_NIGHT_AUTO_BATTERY : MODE_NIGHT_FOLLOW_SYSTEM);
 
